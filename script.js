28783453c2f7357a4c8aa2e3b180ef9dba60e9e6
Treaty <135383414+treaty1210@users.noreply.github.com>
2023-06-13 15:00:22 -0700

    Add reset button
    Rewrite createGrid to a function that will take in number input from
    user.
    
    Remove width and height in style.css and add that to script.js and make
    it adjust to user input.
    
    Move hover effect to createGrid.
    
    Create button that will prompt for number input and run createGrid.
    Any number above 100 will cause prompt to ask for number equal or less
    than 100.
    
    Create new removeGrid div to reset grid when button is pressed.

--- script.js
+++ script.js
@@ -8,17 +8,55 @@ const container = document.createElement("div");
     container.classList.add("container");
     body.appendChild(container);
 
-//Grid
-for (i = 0; i < 256; i++) {
-    const grid = document.createElement("div");
-    grid.classList.add("grid");
-    container.appendChild(grid);
+//Grid: create function to work with button and different lengths
+function createGrid(gridSize) {
+    //Div that will be used to reset grid
+    const removeGrid = document.createElement("div")
+    removeGrid.classList.add("removeGrid")
+
+    for (i = 0; i < gridSize; i++) {
+        const row = document.createElement("div");
+        row.classList.add("grid-row");
+        //this will create squares in the rows
+        for (s = 0; s < gridSize; s++) {
+            const widthAndHeight = 1000 / gridSize;
+            const grid = document.createElement("div");
+            grid.classList.add("grid")
+                grid.style.width = `${widthAndHeight}px`;
+                grid.style.height = `${widthAndHeight}px`;
+            //Move hover effect eventlistener here
+            grid.addEventListener("mouseover", () => {
+                grid.style.backgroundColor = "aquamarine";
+                }) 
+            row.appendChild(grid);
+        } 
+        removeGrid.appendChild(row);
+    } 
+    container.appendChild(removeGrid);
 };
 
-//Hover effect to change color
-const box = document.querySelectorAll(".grid");
-box.forEach(grid => {
-    grid.addEventListener("mouseover", () => {
-    grid.style.backgroundColor = "aquamarine";
-    }) 
+//Needs to be here or removeGrid div will not be created leading to null value
+createGrid(0);
+
+//Button
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    let newSize = Number(prompt("Choose a number to get new grid size. (E.g. 5 creates a 5x5 box.)"));
+
+    if (newSize > 100) {
+        newSize = Number(prompt("Please choose a number less than 100."));
+    }
+    //Should remove any existing grid from page
+    const removeGrid = document.querySelector(".removeGrid");
+    removeGrid.remove();
+    //Runs function after removing existing grid to create blank grid
+    createGrid(newSize);
 });
+
+//Hover effect to change color
+// const box = document.querySelectorAll(".grid");
+// box.forEach(grid => {
+//     grid.addEventListener("mouseover", () => {
+//     grid.style.backgroundColor = "aquamarine";
+//     }) 
+// });
